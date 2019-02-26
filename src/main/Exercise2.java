62d5d8ab472d09cfd98e6ad99b85a16f47981125
deeksha kashyap <deekshakas>
2019-02-26 22:46:17 +0530

    insert int type variable array,array

--- src/main/Exercise2.java
+++ src/main/Exercise2.java
@@ -2,6 +2,10 @@ package main;
 
 public class Exercise2 {
     public static void main(String[] args) {
+      int[] array={1,2,3,4,5,6,7,8,9};
+      int sum=0;
+        for (int i : array) {
 
+        }
     }
 }

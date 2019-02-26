9dc6b12a43be5d564b3f464965beb5f9404d2521
deeksha kashyap <deekshakas>
2019-02-26 22:24:49 +0530

    add String variable ,array2

--- src/main/Exercise1.java
+++ src/main/Exercise1.java
@@ -1,7 +1,16 @@
 package main;
 
+import java.util.Arrays;
+
 public class Exercise1 {
     public static void main(String[] args) {
        int[] array1={123,234,345,456,567,678,890};
+       String[] array2={"java","python","C#","C programme","C sharp"};
+        System.out.println("Original numeric array :"+ Arrays.toString(array1));
+        Arrays.sort(array1);
+        System.out.println("Sorted numeric array :"+Arrays.toString(array1));
+        System.out.println("Original numeric array :"+ Arrays.toString(array2));
+        Arrays.sort(array2);
+        System.out.println("Sorted numeric array :"+Arrays.toString(array2));
     }
 }
